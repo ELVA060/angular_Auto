2488b673b15beeec7597a4b9a64b9d16842c7532
elva <elvacucuri@gmail.com>
2024-03-27 08:20:18 -0500

    creacion interface autos

---
+++ src/app/interfaceautos/autos.ts
@@ -0,0 +1,14 @@
+export interface Autos {
+
+    image:string;
+    codigo:string;
+    marca:string;
+    modelo: string;
+    anio: number;
+    color?:string;
+    kilometraje:number;
+    precio:string;
+    calificacion: number;
+        
+    }
+    
