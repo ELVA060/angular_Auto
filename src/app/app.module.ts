67de80903cfc387f867afcc61393099ac2e1ad8a
elva <elvacucuri@gmail.com>
2024-03-27 08:26:55 -0500

    importacion y declaracion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginalistavehiculosComponent } from './paginas/paginalistavehiculos/paginalistavehiculos.component';
+import { IniciopaginaComponent } from './paginas/iniciopagina/iniciopagina.component';
+import { DetallevehiculosComponent } from './paginas/detallevehiculos/detallevehiculos.component';
+import { EliminarvehiculoComponent } from './paginas/eliminarvehiculo/eliminarvehiculo.component';
+import { EditarvehiculoComponent } from './paginas/editarvehiculo/editarvehiculo.component';
+import { EstrellasComponent } from './utilitarios/estrellas/estrellas.component';
+import { RegistrolitavehiculosComponent } from './paginas/registrolitavehiculos/registrolitavehiculos.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,PaginalistavehiculosComponent,IniciopaginaComponent,DetallevehiculosComponent,EliminarvehiculoComponent,EditarvehiculoComponent,EstrellasComponent,RegistrolitavehiculosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule, RouterModule, ReactiveFormsModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servicios/servehiculos.service.ts
+++ src/app/servicios/servehiculos.service.ts
@@ -1,10 +1,56 @@
 import { Injectable } from '@angular/core';
+import { Autos } from '../interfaceautos/autos';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServehiculosService {
-
+  servlistauto: Autos[] = [{
+    image: "https://www.chevrolet.com.ec/bypass/gmccontenthub/chevrolet/gm-ec/colorizer_joy_sedan/images/colorizer/joy-hb-negro-3-4F.png",
+    codigo: "A001",
+    marca: "Chevrolet",
+    modelo: "Joy Sedán",
+    anio: 2024,
+    color: "negro",
+    kilometraje: 0,
+    precio: "17000",
+    calificacion: 2,
+  },
+  {
+    image: "https://assets-cdn.static-gm.com/Assets/83ec4933-56df-41a8-92b1-5366014876c6/a5908a34-214c-4f0c-b419-23933629d49d/v-1656471565/Desktop.webp",
+    codigo: "A002",
+    marca: "Chevrolet",
+    modelo: "Onix Turbo RS",
+    anio: 2024,
+    color: "blanco",
+    kilometraje: 0,
+    precio: "17000",
+    calificacion: 4,
+  },
+  {
+    image: "https://vehicle-images.dealerinspire.com/stock-images/ford/5689dc835ebd59a2fd5a637d0e1bc678.png",
+    codigo: "A003",
+    marca: "Ford",
+    modelo: "Escape platinum",
+    anio: 2024,
+    color: "negro",
+    kilometraje: 0,
+    precio: "20000",
+    calificacion: 4,
+  },
+  {
+    image: "https://www.toyocosta.com/wp-content/uploads/2022/08/raize-plata.png",
+    codigo: "A003",
+    marca: "Toyota",
+    modelo: "Raize",
+    anio: 2023,
+    color: "plata",
+    kilometraje: 0,
+    precio: "22000",
+    calificacion: 2,
+  },
+  ]
 constructor() { }
 
 }
